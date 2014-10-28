f2a55d65e9fb8f76c5f9fc986dd6db28ac8cbbaa
knock-- <root@black-hand.com>
2014-10-27 20:14:00 -0500

    Extend screen dimensions testing

--- js/tests/tests.js
+++ js/tests/tests.js
@@ -18,14 +18,66 @@
 		(function () {
 			q.module("Screen Dimensions");
 			q.test("Screen Height", function (assert) {
-				assert.expect(2);
-				assert.ok($.getScreenHeight);
-				assert.notStrictEqual($.getScreenHeight(), null, "Method should not return null");
+				assert.expect(6);
+				assert.ok(
+					$.setScreenHeight,
+					"Assert method exists"
+				);
+				assert.ok(
+					$.getScreenHeight,
+					"Assert method exists"
+				);
+				assert.notStrictEqual(
+					$.setScreenHeight(),
+					null,
+					"Assert method should not return null"
+				);
+				assert.notStrictEqual(
+					$.getScreenHeight(),
+					null,
+					"Assert method should not return null"
+				);
+				assert.strictEqual(
+					$.setScreenHeight(),
+					undefined,
+					"Assert method should return undefined"
+				);
+				assert.strictEqual(
+					$.getScreenHeight(),
+					$(window).height(),
+					"Assert method should return window height as integer"
+				);
 			});
 			q.test("Screen Width", function (assert) {
-				assert.expect(2);
-				assert.ok($.getScreenWidth);
-				assert.notStrictEqual($.getScreenWidth(), null, "Method should not return null");
+				assert.expect(6);
+				assert.ok(
+					$.setScreenWidth,
+					"Assert method exists"
+				);
+				assert.ok(
+					$.getScreenWidth,
+					"Assert method exists"
+				);
+				assert.notStrictEqual(
+					$.setScreenWidth(),
+					null,
+					"Assert method should not return null"
+				);
+				assert.notStrictEqual(
+					$.getScreenWidth(),
+					null,
+					"Assert method should not return null"
+				);
+				assert.strictEqual(
+					$.setScreenWidth(),
+					undefined,
+					"Assert method should return undefined"
+				);
+				assert.strictEqual(
+					$.getScreenWidth(),
+					$(window).width(),
+					"Assert method should return window width as integer"
+				);
 			});
 		}(makeReady()));
 	});
