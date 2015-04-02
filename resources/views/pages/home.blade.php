29b6e946e05f409d4271d1fbc7b82830fcbad9e8
knock-- <tcoukoulis@redglue.com>
2015-04-01 23:36:35 -0500

    Play with templating engine

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,14 @@
+@extends('app')
+
+@section('content')
+
+	<h1>Welcome</h1>
+
+@endsection
+
+@section('footer')
+	<p>Here's the footer as ordered.</p>
+	<!-- Scripts -->
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+@endsection
